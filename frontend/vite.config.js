0bcdd6ba754e914d56a960f27f4cf21c7faf18b1
markmullin <mullinmark05@gmail.com>
2025-02-22 21:05:40 -0500

    Update vite config for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Updated to match backend port
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
     },
   },
+  base: '/'
 })
